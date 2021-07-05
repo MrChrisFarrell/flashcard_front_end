1da4a0fb72d8cf33ae8aaee6fa78d14c897908a7
unknown <mrchrisfarrell@yahoo.com>
2021-07-05 16:51:48 -0400

    displayFlashcards is breaking the app, can't read term

--- src/Components/App/app.jsx
+++ src/Components/App/app.jsx
@@ -6,16 +6,20 @@ import DisplayCollections from '../Display_collections/displayCollections';
 
 function App() {
     const [collections, setCollections] = useState([]);
+    const [collectionIndex, setCollectionIndex] = useState(1);
+    const [flashcardIndex, setFlashcardIndex] = useState(0);
 
     useEffect(async()=> {
         let response = await axios.get('http://127.0.0.1:8000/collections/');
-        setCollections(response.data)
-    });
+        setCollections(response.data);
+    },);
+
+
 
     return(
             <div>
                 <DisplayCollections collections={collections}/>
-
+                <DisplayFlashcard collectionIndex={collectionIndex} flashcardIndex={flashcardIndex}/>
             </div>
     );
   }
--- src/Components/Display_Flashcard/displayFlashcard.jsx
+++ src/Components/Display_Flashcard/displayFlashcard.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './displayFlashcard.css';
 
-function DisplayFlashcard(){
+const DisplayFlashcard=(props)=>{
+    const [collection, setCollection] = useState([]);
+    setCollection(async()=>{
+        let response = await axios.get(`http://127.0.0.1:8000/flashcards/`, {fk: props.collectionIndex});
+        return response.data;
+})
+
+    useEffect(async()=>{
+
+    });
+
     return(
         <div onClick="application.goToTargetView(event)" id="Flashcard">
-            <h1 className="text">Term</h1>
+            <h1 className="text">{collection[props.flashcardIndex].term}</h1>
         </div>
     )
 }
