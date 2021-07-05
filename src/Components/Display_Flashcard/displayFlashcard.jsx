4a827bf20cea71c1247f88cebd46994a91f9ba06
unknown <mrchrisfarrell@yahoo.com>
2021-07-05 12:44:19 -0400

    flashcard component skeleton done

--- src/Components/Display_Flashcard/displayFlashcard.jsx
+++ src/Components/Display_Flashcard/displayFlashcard.jsx
@@ -3,10 +3,8 @@ import './displayFlashcard.css';
 
 function DisplayFlashcard(){
     return(
-        <div onClick="application.goToTargetView(event)" id="Flashcard" className="Flashcard">
-            <svg className="Flashcard_i">
-                <rect onClick="application.goToTargetView(event)" id="Flashcard_i" rx="64" ry="64" x="0" y="0" width="558" height="624"></rect>
-            </svg>
+        <div onClick="application.goToTargetView(event)" id="Flashcard">
+            <h1 className="text">Term</h1>
         </div>
     )
 }
